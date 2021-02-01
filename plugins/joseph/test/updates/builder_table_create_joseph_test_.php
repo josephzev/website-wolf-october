37e6ac4cadd1f211d2f658d5865f95bcfd0982b8
root <yjotkowitz@gmail.com.com>
2021-02-01 11:43:16 +0700

    first commit

---
+++ plugins/joseph/test/updates/builder_table_create_joseph_test_.php
@@ -0,0 +1,22 @@
+<?php namespace Joseph\Test\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateJosephTest extends Migration
+{
+    public function up()
+    {
+        Schema::create('joseph_test_', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('name');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('joseph_test_');
+    }
+}
